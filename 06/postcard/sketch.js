cb58a121d505f8c13a0fb1e2b8af02131af4e774
Stefan Vögeli <stefan.voegeli@srf.ch>
2023-10-31 21:30:25 +0100

    add sketch with moving eyes

--- 06/postcard/sketch.js
+++ 06/postcard/sketch.js
@@ -1,7 +1,52 @@
+let pointsForEye = [];
+let eyeRadius = 50;
+let eyePoints = 20;
+let eyeEllipseSizeMin = 10;
+let eyeEllipseSizeMax = 100;
 function setup() {
-    createCanvas(400, 400);
+    pointsForEye = calculatePointsOfEllipse(eyePoints, eyeRadius);
+    createCanvas(windowWidth, windowHeight);
+    background(220);
+    angleMode(DEGREES);
+    strokeWeight(2);
 }
 
 function draw() {
-    background(220);
+    background(255)
+    push();
+    translate(width * 0.5 - eyeRadius * 2, height * 0.25);
+    for (let eyePoint of pointsForEye) {
+        strokeWeight(noise((millis() + 9999) * eyePoint.x * eyePoint.y * 0.0000005) * 100)
+        let randomOffsetX = (noise((millis() + 9999) * eyePoint.x * eyePoint.y * 0.0000001) - 0.5) * 70;
+        let randomOffsetY = (noise((millis() + 9999) * eyePoint.x * eyePoint.y * 0.0000001) - 0.5) * 70;
+        point(eyePoint.x + randomOffsetX, eyePoint.y + randomOffsetY);
+    }
+    pop();
+    push();
+    translate(width * 0.5 + eyeRadius * 2, height * 0.25);
+    for (let eyePoint of pointsForEye) {
+        strokeWeight(noise((millis() + 999999) * eyePoint.x * eyePoint.y * 0.0000005) * 100)
+        let randomOffsetX = (noise((millis() + 999999) * eyePoint.x * eyePoint.y * 0.0000001) - 0.5) * 70;
+        let randomOffsetY = (noise((millis() + 999999) * eyePoint.x * eyePoint.y * 0.0000001) - 0.5) * 70;
+        point(eyePoint.x + randomOffsetX, eyePoint.y + randomOffsetY);
+    }
+    pop();
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+// Calculate points for an ellipse based on the amount of points and the radius
+function calculatePointsOfEllipse(amountOfPoints, ellipseRadius) {
+    let points = [];
+    let diameter = ellipseRadius * 2;
+    let circumference = diameter * PI;
+    let circumferenceLengthPerPoint = circumference / amountOfPoints;
+    for (let i = 0; i < amountOfPoints; i++) {
+        let currentArc = circumferenceLengthPerPoint * (i + 1);
+        let currentAngle = map(currentArc, 0, circumference, 0, 360);
+        points.push({ x: sin(currentAngle) * ellipseRadius, y: cos(currentAngle) * ellipseRadius });
+    }
+    return points;
 }
