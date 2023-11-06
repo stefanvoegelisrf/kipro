97c2856c70a002868d85d31129ba0a4d742e44d4
Stefan Vögeli <stefan.voegeli@srf.ch>
2023-11-06 18:53:39 +0100

    add mouth

--- 06/postcard/sketch.js
+++ 06/postcard/sketch.js
@@ -2,12 +2,15 @@ let pointsForEye = [];
 let pointsForLeftNoseWing = [];
 let pointsForRightNoseWing = [];
 let pointsForNoseTip = [];
+let pointsForMouth = [];
 let eyeRadius = 80;
-let eyePoints = 20;
+let eyePoints = 50;
 let noseWingRadius = 40
 let noseWingPoints = 10
 let noseTipPoints = 10;
 let noseTipRadius = 30;
+let mouthPoints = 40;
+let mouthRadius = 200;
 let eyeEllipseSizeMin = 10;
 let eyeEllipseSizeMax = 100;
 function setup() {
@@ -16,6 +19,7 @@ function setup() {
     pointsForLeftNoseWing = calculatePointsOfEllipse(noseWingPoints, noseWingRadius, 190, 370)
     pointsForRightNoseWing = calculatePointsOfEllipse(noseWingPoints, noseWingRadius, -20, 160)
     pointsForNoseTip = calculatePointsOfEllipse(noseTipPoints, noseTipRadius, -100, 80)
+    pointsForMouth = calculatePointsOfEllipse(mouthPoints, mouthRadius, -90, 90)
     console.log(pointsForLeftNoseWing)
     createCanvas(windowWidth, windowHeight);
     background(220);
@@ -26,7 +30,7 @@ function draw() {
     background(255)
     // Left eye
     push();
-    translate(width * 0.5 - eyeRadius * 2, height * 0.25);
+    translate(width * 0.5 - eyeRadius * 1.5, height * 0.25);
     for (let eyePoint of pointsForEye) {
         point(eyePoint.x, eyePoint.y);
     }
@@ -34,7 +38,7 @@ function draw() {
 
     // Right eye
     push();
-    translate(width * 0.5 + eyeRadius * 2, height * 0.25);
+    translate(width * 0.5 + eyeRadius * 1.5, height * 0.25);
     for (let eyePoint of pointsForEye) {
         point(eyePoint.x, eyePoint.y);
     }
@@ -56,11 +60,19 @@ function draw() {
     }
     pop();
 
-    // Right nose wing
+    // Nose tip
     push();
     translate(width * 0.5, height * 0.5 + noseTipRadius);
     for (let noseTipPoint of pointsForNoseTip) {
         point(noseTipPoint.x, noseTipPoint.y);
+    }
+    pop();
+
+    // Mouth
+    push();
+    translate(width * 0.5, height * 0.6);
+    for (let mouthPoint of pointsForMouth) {
+        point(mouthPoint.x, mouthPoint.y);
     }
     pop();
 }
