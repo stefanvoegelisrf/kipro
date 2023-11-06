f2a1be1dc3c0d659a2d4e03732d83884dda50045
Stefan Vögeli <stefan.voegeli@srf.ch>
2023-11-06 20:45:21 +0100

    add guid, change to instance mode

--- 06/postcard/sketch.js
+++ 06/postcard/sketch.js
@@ -1,96 +1,109 @@
-let pointsForEye = [];
-let pointsForLeftNoseWing = [];
-let pointsForRightNoseWing = [];
-let pointsForNoseTip = [];
-let pointsForMouth = [];
-let eyeRadius = 80;
-let eyePoints = 50;
-let noseWingRadius = 40
-let noseWingPoints = 10
-let noseTipPoints = 10;
-let noseTipRadius = 60;
-let mouthPoints = 40;
-let mouthRadius = 200;
-let eyeEllipseSizeMin = 10;
-let eyeEllipseSizeMax = 100;
-function setup() {
-    angleMode(DEGREES);
-    pointsForEye = calculatePointsOfEllipse(eyePoints, eyeRadius, 0, 360);
-    pointsForLeftNoseWing = calculatePointsOfEllipse(noseWingPoints, noseWingRadius, 190, 370)
-    pointsForRightNoseWing = calculatePointsOfEllipse(noseWingPoints, noseWingRadius, -20, 160)
-    pointsForNoseTip = calculatePointsOfEllipse(noseTipPoints, noseTipRadius, -100, 80)
-    pointsForMouth = calculatePointsOfEllipse(mouthPoints, mouthRadius, -90, 90)
-    console.log(pointsForLeftNoseWing)
-    createCanvas(windowWidth, windowHeight);
-    background(220);
-    strokeWeight(5);
-}
-
-function draw() {
-    background(255)
-    // Left eye
-    push();
-    translate(width * 0.5 - eyeRadius * 1.5, height * 0.25);
-    for (let eyePoint of pointsForEye) {
-        point(eyePoint.x, eyePoint.y);
+import { GUI } from './lil-gui.esm.min.js';
+export default function sketch(p5) {
+    let settings = {
+        eyeRadius: 80,
+        eyePoints: 50,
+        noseWingRadius: 40,
+        noseWingPoints: 10,
+        noseTipPoints: 10,
+        noseTipRadius: 60,
+        mouthPoints: 40,
+        mouthRadius: 200
     }
-    pop();
-
-    // Right eye
-    push();
-    translate(width * 0.5 + eyeRadius * 1.5, height * 0.25);
-    for (let eyePoint of pointsForEye) {
-        point(eyePoint.x, eyePoint.y);
+    let pointsForEye = [];
+    let pointsForLeftNoseWing = [];
+    let pointsForRightNoseWing = [];
+    let pointsForNoseTip = [];
+    let pointsForMouth = [];
+    let eyeRadius = 80;
+    let eyePoints = 50;
+    let noseWingRadius = 40
+    let noseWingPoints = 10
+    let noseTipPoints = 10;
+    let noseTipRadius = 60;
+    let mouthPoints = 40;
+    let mouthRadius = 200;
+    p5.setup = function () {
+        let customizeSketchGui = new GUI();
+        customizeSketchGui.add(settings, 'eyeRadius', 0, 200);
+        p5.angleMode(p5.DEGREES);
+        p5.createCanvas(p5.windowWidth, p5.windowHeight);
+        p5.background(220);
+        p5.strokeWeight(5);
     }
-    pop();
 
-    // Left nose wing
-    push();
-    translate(width * 0.5 - noseWingRadius * 2, height * 0.5);
-    for (let leftNoseWingPoint of pointsForLeftNoseWing) {
-        point(leftNoseWingPoint.x, leftNoseWingPoint.y);
+    p5.draw = function () {
+        pointsForEye = calculatePointsOfEllipse(settings.eyePoints, settings.eyeRadius, 0, 360);
+        pointsForLeftNoseWing = calculatePointsOfEllipse(settings.noseWingPoints, settings.noseWingRadius, 190, 370)
+        pointsForRightNoseWing = calculatePointsOfEllipse(settings.oseWingPoints, settings.noseWingRadius, -20, 160)
+        pointsForNoseTip = calculatePointsOfEllipse(settings.noseTipPoints, settings.noseTipRadius, -100, 80)
+        pointsForMouth = calculatePointsOfEllipse(settings.mouthPoints, settings.mouthRadius, -90, 90)
+        p5.background(255)
+        // Left eye
+        p5.push();
+        p5.translate(p5.width * 0.5 - settings.eyeRadius * 1.5, p5.height * 0.25);
+        for (let eyePoint of pointsForEye) {
+            p5.point(eyePoint.x, eyePoint.y);
+        }
+        p5.pop();
+
+        // Right eye
+        p5.push();
+        p5.translate(p5.width * 0.5 + settings.eyeRadius * 1.5, p5.height * 0.25);
+        for (let eyePoint of pointsForEye) {
+            p5.point(eyePoint.x, eyePoint.y);
+        }
+        p5.pop();
+
+        // Left nose wing
+        p5.push();
+        p5.translate(p5.width * 0.5 - settings.noseWingRadius * 2, p5.height * 0.5);
+        for (let leftNoseWingPoint of pointsForLeftNoseWing) {
+            p5.point(leftNoseWingPoint.x, leftNoseWingPoint.y);
+        }
+        p5.pop();
+
+        // Right nose wing
+        p5.push();
+        p5.translate(p5.width * 0.5 + settings.noseWingRadius * 2, p5.height * 0.5);
+        for (let rightNoseWingPoint of pointsForRightNoseWing) {
+            p5.point(rightNoseWingPoint.x, rightNoseWingPoint.y);
+        }
+        p5.pop();
+
+        // Nose tip
+        p5.push();
+        p5.translate(p5.width * 0.5, p5.height * 0.5 + settings.noseTipRadius * 0.5);
+        for (let noseTipPoint of pointsForNoseTip) {
+            p5.point(noseTipPoint.x, noseTipPoint.y);
+        }
+        p5.pop();
+
+        // Mouth
+        p5.push();
+        p5.translate(p5.width * 0.5, p5.height * 0.6);
+        for (let mouthPoint of pointsForMouth) {
+            p5.point(mouthPoint.x, mouthPoint.y);
+        }
+        p5.pop();
     }
-    pop();
 
-    // Right nose wing
-    push();
-    translate(width * 0.5 + noseWingRadius * 2, height * 0.5);
-    for (let rightNoseWingPoint of pointsForRightNoseWing) {
-        point(rightNoseWingPoint.x, rightNoseWingPoint.y);
+    p5.windowResized = function () {
+        p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
     }
-    pop();
 
-    // Nose tip
-    push();
-    translate(width * 0.5, height * 0.5 + noseTipRadius * 0.5);
-    for (let noseTipPoint of pointsForNoseTip) {
-        point(noseTipPoint.x, noseTipPoint.y);
+    // Calculate points for an ellipse based on the amount of points and the radius
+    function calculatePointsOfEllipse(amountOfPoints, ellipseRadius, startAngle, endAngle) {
+        let points = [];
+        let diameter = ellipseRadius * 2;
+        let circumference = diameter * p5.PI;
+        let circumferenceLengthPerPoint = circumference / amountOfPoints;
+        for (let i = 0; i < amountOfPoints; i++) {
+            let currentArc = circumferenceLengthPerPoint * (i + 1);
+            let currentAngle = p5.map(currentArc, 0, circumference, startAngle, endAngle);
+            points.push({ x: p5.sin(currentAngle) * ellipseRadius, y: p5.cos(currentAngle) * ellipseRadius });
+        }
+        return points;
     }
-    pop();
+};
 
-    // Mouth
-    push();
-    translate(width * 0.5, height * 0.6);
-    for (let mouthPoint of pointsForMouth) {
-        point(mouthPoint.x, mouthPoint.y);
-    }
-    pop();
-}
-
-function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
-}
-
-// Calculate points for an ellipse based on the amount of points and the radius
-function calculatePointsOfEllipse(amountOfPoints, ellipseRadius, startAngle, endAngle) {
-    let points = [];
-    let diameter = ellipseRadius * 2;
-    let circumference = diameter * PI;
-    let circumferenceLengthPerPoint = circumference / amountOfPoints;
-    for (let i = 0; i < amountOfPoints; i++) {
-        let currentArc = circumferenceLengthPerPoint * (i + 1);
-        let currentAngle = map(currentArc, 0, circumference, startAngle, endAngle);
-        points.push({ x: sin(currentAngle) * ellipseRadius, y: cos(currentAngle) * ellipseRadius });
-    }
-    return points;
-}
