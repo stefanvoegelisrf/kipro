ccad051805d7ecf4dee4b424d9f09d97380dfbdc
Stefan Vögeli <stefan.voegeli@srf.ch>
2023-11-07 00:19:24 +0100

    split settings, add random background

--- 06/postcard/sketch.js
+++ 06/postcard/sketch.js
@@ -5,216 +5,233 @@ export default function sketch(p5) {
     let initialWingRadius = 40;
     let initialTipRadius = 60;
     let settings = {
-        righteye: {
-            startAngle: 0,
-            endAngle: 360,
-            radius: initialEyeRadius,
-            pointAmount: 50,
-            pointArray: [],
-            stroke: {
-                color: '#000000',
-                weight: 5,
-                randomize: false,
-                random: {
-                    min: 0,
-                    max: 50
+        faceparts: {
+            righteye: {
+                startAngle: 0,
+                endAngle: 360,
+                radius: initialEyeRadius,
+                pointAmount: 50,
+                pointArray: [],
+                stroke: {
+                    color: '#000000',
+                    weight: 5,
+                    randomize: false,
+                    random: {
+                        min: 0,
+                        max: 50
+                    }
+                },
+                offset: {
+                    x: initialEyeRadius * 1.5,
+                    y: -100
                 }
             },
-            offset: {
-                x: initialEyeRadius * 1.5,
-                y: -100
-            }
-        },
-        lefteye: {
-            startAngle: 0,
-            endAngle: 360,
-            radius: initialEyeRadius,
-            pointAmount: 50,
-            pointArray: [],
-            stroke: {
-                color: '#000000',
-                weight: 5,
-                randomize: false,
-                random: {
-                    min: 0,
-                    max: 50
+            lefteye: {
+                startAngle: 0,
+                endAngle: 360,
+                radius: initialEyeRadius,
+                pointAmount: 50,
+                pointArray: [],
+                stroke: {
+                    color: '#000000',
+                    weight: 5,
+                    randomize: false,
+                    random: {
+                        min: 0,
+                        max: 50
+                    }
+                },
+                offset: {
+                    x: -initialEyeRadius * 1.5,
+                    y: -100
                 }
             },
-            offset: {
-                x: -initialEyeRadius * 1.5,
-                y: -100
-            }
-        },
-        rightpupil: {
-            startAngle: 0,
-            endAngle: 360,
-            radius: initialPupilRadius,
-            pointAmount: 10,
-            pointArray: [],
-            stroke: {
-                color: '#000000',
-                weight: 15,
-                randomize: false,
-                random: {
-                    min: 0,
-                    max: 50
+            rightpupil: {
+                startAngle: 0,
+                endAngle: 360,
+                radius: initialPupilRadius,
+                pointAmount: 10,
+                pointArray: [],
+                stroke: {
+                    color: '#000000',
+                    weight: 15,
+                    randomize: false,
+                    random: {
+                        min: 0,
+                        max: 50
+                    }
+                },
+                offset: {
+                    x: initialEyeRadius * 1.5,
+                    y: -100
                 }
             },
-            offset: {
-                x: initialEyeRadius * 1.5,
-                y: -100
-            }
-        },
-        leftpupil: {
-            startAngle: 0,
-            endAngle: 360,
-            radius: initialPupilRadius,
-            pointAmount: 10,
-            pointArray: [],
-            stroke: {
-                color: '#000000',
-                weight: 15,
-                randomize: false,
-                random: {
-                    min: 0,
-                    max: 50
+            leftpupil: {
+                startAngle: 0,
+                endAngle: 360,
+                radius: initialPupilRadius,
+                pointAmount: 10,
+                pointArray: [],
+                stroke: {
+                    color: '#000000',
+                    weight: 15,
+                    randomize: false,
+                    random: {
+                        min: 0,
+                        max: 50
+                    }
+                },
+                offset: {
+                    x: -initialEyeRadius * 1.5,
+                    y: -100
                 }
             },
-            offset: {
-                x: -initialEyeRadius * 1.5,
-                y: -100
-            }
-        },
-        nosewingleft: {
-            startAngle: 0,
-            endAngle: 150,
-            radius: initialWingRadius,
-            pointAmount: 10,
-            pointArray: [],
-            stroke: {
-                color: '#000000',
-                weight: 5,
-                randomize: false,
-                random: {
-                    min: 0,
-                    max: 50
+            nosewingleft: {
+                startAngle: 0,
+                endAngle: 150,
+                radius: initialWingRadius,
+                pointAmount: 10,
+                pointArray: [],
+                stroke: {
+                    color: '#000000',
+                    weight: 5,
+                    randomize: false,
+                    random: {
+                        min: 0,
+                        max: 50
+                    }
+                },
+                offset: {
+                    x: initialWingRadius * 2,
+                    y: 0
                 }
             },
-            offset: {
-                x: initialWingRadius * 2,
-                y: 0
-            }
-        },
-        nosewingright: {
-            startAngle: 30,
-            endAngle: 180,
-            radius: initialWingRadius,
-            pointAmount: 10,
-            pointArray: [],
-            stroke: {
-                color: '#000000',
-                weight: 5,
-                randomize: false,
-                random: {
-                    min: 0,
-                    max: 50
+            nosewingright: {
+                startAngle: 30,
+                endAngle: 180,
+                radius: initialWingRadius,
+                pointAmount: 10,
+                pointArray: [],
+                stroke: {
+                    color: '#000000',
+                    weight: 5,
+                    randomize: false,
+                    random: {
+                        min: 0,
+                        max: 50
+                    }
+                },
+                offset: {
+                    x: -initialWingRadius * 2,
+                    y: 0
                 }
             },
-            offset: {
-                x: -initialWingRadius * 2,
-                y: 0
-            }
-        },
-        nosetip: {
-            startAngle: 0,
-            endAngle: 180,
-            radius: initialTipRadius,
-            pointAmount: 10,
-            pointArray: [],
-            stroke: {
-                color: '#000000',
-                weight: 5,
-                randomize: false,
-                random: {
-                    min: 0,
-                    max: 50
+            nosetip: {
+                startAngle: 0,
+                endAngle: 180,
+                radius: initialTipRadius,
+                pointAmount: 10,
+                pointArray: [],
+                stroke: {
+                    color: '#000000',
+                    weight: 5,
+                    randomize: false,
+                    random: {
+                        min: 0,
+                        max: 50
+                    }
+                },
+                offset: {
+                    x: 0,
+                    y: initialTipRadius * 0.5
                 }
             },
-            offset: {
-                x: 0,
-                y: initialTipRadius * 0.5
-            }
-        },
-        mouth: {
-            startAngle: 45,
-            endAngle: 135,
-            radius: 300,
-            pointAmount: 40,
-            pointArray: [],
-            stroke: {
-                color: '#000000',
-                weight: 5,
-                randomize: false,
-                random: {
-                    min: 0,
-                    max: 50
+            mouth: {
+                startAngle: 45,
+                endAngle: 135,
+                radius: 300,
+                pointAmount: 40,
+                pointArray: [],
+                stroke: {
+                    color: '#000000',
+                    weight: 5,
+                    randomize: false,
+                    random: {
+                        min: 0,
+                        max: 50
+                    }
+                },
+                offset: {
+                    x: 0,
+                    y: -100
                 }
             },
-            offset: {
-                x: 0,
-                y: -100
-            }
         },
+        background: {
+            color: '#FFFFFF',
+            randomize: false,
+        }
     }
-    const randomizeSubsetting=(settings)=>{
-        for(let settingsKey in settings){
-            if(typeof settings[settingsKey] === 'object'){
-                randomizeSubsetting(settings[settingsKey]);
+
+    const randomHexColor = () => {
+        return '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
+    }
+
+    const randomizeSubsetting = (subsetting) => {
+        for (let settingsKey in subsetting) {
+            if (typeof subsetting[settingsKey] === 'object') {
+                randomizeSubsetting(subsetting[settingsKey]);
             }
-            else if(typeof settings[settingsKey] === 'number'){
-                let lowercaseSettingsKey=settingsKey.toLowerCase();
+            else if (typeof subsetting[settingsKey] === 'number') {
+                let lowercaseSettingsKey = settingsKey.toLowerCase();
                 // Check if settingsKey ends with amount
-                if(lowercaseSettingsKey.endsWith('amount')){
-                    settings[settingsKey] = p5.random(0, 100);
+                if (lowercaseSettingsKey.endsWith('amount')) {
+                    subsetting[settingsKey] = p5.random(0, 100);
                     continue;
                 }
                 // Check if settingsKey ends with radius
-                if(lowercaseSettingsKey.endsWith('radius')){
-                    settings[settingsKey] = p5.random(0, 300);
+                if (lowercaseSettingsKey.endsWith('radius')) {
+                    subsetting[settingsKey] = p5.random(0, 300);
                     continue;
                 }
                 // Check if settingsKey ends with weight
-                if(lowercaseSettingsKey.endsWith('weight')){
-                    settings[settingsKey] = p5.random(0, 20);
+                if (lowercaseSettingsKey.endsWith('weight')) {
+                    subsetting[settingsKey] = p5.random(0, 20);
                     continue;
                 }
                 // Check if settingsKey ends with angle
-                if(lowercaseSettingsKey.endsWith('angle')){
-                    settings[settingsKey] = p5.random(0, 360);
+                if (lowercaseSettingsKey.endsWith('angle')) {
+                    subsetting[settingsKey] = p5.random(0, 360);
                     continue;
                 }
                 // Check if settingsKey ends with x
-                if(lowercaseSettingsKey.endsWith('x')){
-                    settings[settingsKey] = p5.random(-300, 300);
+                if (lowercaseSettingsKey.endsWith('x')) {
+                    subsetting[settingsKey] = p5.random(-300, 300);
                     continue;
                 }
                 // Check if settingsKey ends with y
-                if(lowercaseSettingsKey.endsWith('y')){
-                    settings[settingsKey] = p5.random(-300, 300);
+                if (lowercaseSettingsKey.endsWith('y')) {
+                    subsetting[settingsKey] = p5.random(-300, 300);
                     continue;
                 }
             }
-            else if(typeof settings[settingsKey] === 'string'){
-                settings[settingsKey] = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+            else if (typeof subsetting[settingsKey] === 'string') {
+                subsetting[settingsKey] = randomHexColor();
             }
-            else if(typeof settings[settingsKey] === 'boolean'){
-                settings[settingsKey] = p5.random(0, 1) > 0.5;
+            else if (typeof settings[settingsKey] === 'boolean') {
+                subsetting[settingsKey] = p5.random(0, 1) > 0.5;
             }
         }
     }
 
     p5.setup = function () {
         let customizeSketchGui = new GUI();
+        const backgroundGui = customizeSketchGui.addFolder('Background');
+        backgroundGui.open(false);
+        backgroundGui.addColor(settings.background, 'color');
+        const randomFolder = backgroundGui.addFolder('Random');
+        randomFolder.open(false);
+        randomFolder.add(settings.background, 'randomize');
         let actions = {
             savePreset() {
                 const preset = customizeSketchGui.save();
@@ -228,32 +245,32 @@ export default function sketch(p5) {
                     customizeSketchGui.load(preset);
                 });
             },
-            randomizeSettings(){
-                for (let key in settings) {
-                    randomizeSubsetting(settings[key]);
+            randomizeSettings() {
+                for (let key in settings.faceparts) {
+                    randomizeSubsetting(settings.faceparts[key]);
                 }
             }
         }
-        for (let key in settings) {
+        for (let key in settings.faceparts) {
             let keyGui = customizeSketchGui.addFolder(key);
             keyGui.open(false);
-            keyGui.add(settings[key], 'startAngle', 0, 360);
-            keyGui.add(settings[key], 'endAngle', 0, 360);
-            keyGui.add(settings[key], 'radius', 0, 300);
-            keyGui.add(settings[key], 'pointAmount', 0, 100).step(1);
+            keyGui.add(settings.faceparts[key], 'startAngle', 0, 360);
+            keyGui.add(settings.faceparts[key], 'endAngle', 0, 360);
+            keyGui.add(settings.faceparts[key], 'radius', 0, 300);
+            keyGui.add(settings.faceparts[key], 'pointAmount', 0, 100).step(1);
             const strokeGui = keyGui.addFolder('Stroke');
             strokeGui.open(false);
-            strokeGui.addColor(settings[key].stroke, 'color');
-            strokeGui.add(settings[key].stroke, 'weight', 0, 20);
+            strokeGui.addColor(settings.faceparts[key].stroke, 'color');
+            strokeGui.add(settings.faceparts[key].stroke, 'weight', 0, 20);
             const randomGui = strokeGui.addFolder('Random');
             randomGui.open(false);
-            randomGui.add(settings[key].stroke, 'randomize');
-            randomGui.add(settings[key].stroke.random, 'min', 0, 50);
-            randomGui.add(settings[key].stroke.random, 'max', 0, 50);
+            randomGui.add(settings.faceparts[key].stroke, 'randomize');
+            randomGui.add(settings.faceparts[key].stroke.random, 'min', 0, 50);
+            randomGui.add(settings.faceparts[key].stroke.random, 'max', 0, 50);
             const offsetGui = keyGui.addFolder('Offset');
             offsetGui.open(false);
-            offsetGui.add(settings[key].offset, 'x', -300, 300);
-            offsetGui.add(settings[key].offset, 'y', -300, 300);
+            offsetGui.add(settings.faceparts[key].offset, 'x', -300, 300);
+            offsetGui.add(settings.faceparts[key].offset, 'y', -300, 300);
         }
         customizeSketchGui.add(actions, 'savePreset').name('Save to clipboard');
         customizeSketchGui.add(actions, 'loadPreset').name('Load from clipboard');
@@ -266,14 +283,14 @@ export default function sketch(p5) {
     }
 
     p5.draw = function () {
-        p5.background(255)
-        for (let key in settings) {
-            settings[key].pointArray = calculatePointsOfEllipse(settings[key].pointAmount, settings[key].radius, settings[key].startAngle, settings[key].endAngle);
+        p5.background(settings.background.randomize ? randomHexColor() : settings.background.color)
+        for (let key in settings.faceparts) {
+            settings.faceparts[key].pointArray = calculatePointsOfEllipse(settings.faceparts[key].pointAmount, settings.faceparts[key].radius, settings.faceparts[key].startAngle, settings.faceparts[key].endAngle);
             p5.push();
-            p5.stroke(settings[key].stroke.color);
-            p5.translate(p5.width * 0.5 + settings[key].offset.x, p5.height * 0.5 + settings[key].offset.y);
-            for (let point of settings[key].pointArray) {
-                p5.strokeWeight(settings[key].stroke.randomize ? p5.random(settings[key].stroke.random.min, settings[key].stroke.random.max) : settings[key].stroke.weight);
+            p5.stroke(settings.faceparts[key].stroke.color);
+            p5.translate(p5.width * 0.5 + settings.faceparts[key].offset.x, p5.height * 0.5 + settings.faceparts[key].offset.y);
+            for (let point of settings.faceparts[key].pointArray) {
+                p5.strokeWeight(settings.faceparts[key].stroke.randomize ? p5.random(settings.faceparts[key].stroke.random.min, settings.faceparts[key].stroke.random.max) : settings.faceparts[key].stroke.weight);
                 p5.point(point.x, point.y);
             }
             p5.pop();
