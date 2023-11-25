356ae8e0f47c3434ca1de08ec00dd28830e1864d
Stefan Vögeli <stefan.voegeli@srf.ch>
2023-11-25 23:38:23 +0100

    add settings for rig

--- end project/clock lomepal stage design/src/sketch.ts
+++ end project/clock lomepal stage design/src/sketch.ts
@@ -48,7 +48,7 @@ const drawLightTube = (sketch: p5, x: number, y: number, width: number, height: 
     sketch.pop();
 }
 
-const drawLightingRig = (sketch: p5, tubeLightGlowColor: { cssColor: string }) => {
+const drawLightingRig = (sketch: p5, tubeLightGlowColor: string) => {
     // draw dark grey lighting brackets
     // lighting brackets are square and divided into 4 sections
     let rectangleWidth = 400;
@@ -101,19 +101,19 @@ const drawLightingRig = (sketch: p5, tubeLightGlowColor: { cssColor: string }) =
     // draw vertical light tubes
     let verticalLightTubes = [
         // left side
-        new LightTube(rectangleStart, rectangleStart, tubeWidth, rectangleWidth * 0.3, tubeLightGlowColor.cssColor, cornerRadius),
-        new LightTube(rectangleStart, rectangleWidth * 0.3, tubeWidth, rectangleWidth * 0.3, tubeLightGlowColor.cssColor, cornerRadius),
+        new LightTube(rectangleStart, rectangleStart, tubeWidth, rectangleWidth * 0.3, tubeLightGlowColor, cornerRadius),
+        new LightTube(rectangleStart, rectangleWidth * 0.3, tubeWidth, rectangleWidth * 0.3, tubeLightGlowColor, cornerRadius),
         // left middle
-        new LightTube(-rectangleWidth * 0.25, -rectangleWidth * 0.1, tubeWidth, rectangleWidth * 0.5, tubeLightGlowColor.cssColor, cornerRadius),
-        new LightTube(-rectangleWidth * 0.25, -rectangleStart, tubeWidth, rectangleWidth * 0.3, tubeLightGlowColor.cssColor, cornerRadius),
+        new LightTube(-rectangleWidth * 0.25, -rectangleWidth * 0.1, tubeWidth, rectangleWidth * 0.5, tubeLightGlowColor, cornerRadius),
+        new LightTube(-rectangleWidth * 0.25, -rectangleStart, tubeWidth, rectangleWidth * 0.3, tubeLightGlowColor, cornerRadius),
         // middle
-        new LightTube(0, rectangleStart, tubeWidth, rectangleWidth * 0.15, tubeLightGlowColor.cssColor, cornerRadius),
-        new LightTube(0, 0, tubeWidth, rectangleWidth * 0.3, tubeLightGlowColor.cssColor, cornerRadius),
-        new LightTube(0, -rectangleStart + 50, tubeWidth, rectangleWidth * 0.15, tubeLightGlowColor.cssColor, cornerRadius),
+        new LightTube(0, rectangleStart, tubeWidth, rectangleWidth * 0.15, tubeLightGlowColor, cornerRadius),
+        new LightTube(0, 0, tubeWidth, rectangleWidth * 0.3, tubeLightGlowColor, cornerRadius),
+        new LightTube(0, -rectangleStart + 50, tubeWidth, rectangleWidth * 0.15, tubeLightGlowColor, cornerRadius),
         // right middle
-        new LightTube(rectangleWidth * 0.25, -rectangleStart * 0.6, tubeWidth, rectangleWidth * 0.5, tubeLightGlowColor.cssColor, cornerRadius),
+        new LightTube(rectangleWidth * 0.25, -rectangleStart * 0.6, tubeWidth, rectangleWidth * 0.5, tubeLightGlowColor, cornerRadius),
         // right side
-        new LightTube(-rectangleStart, -rectangleStart * 0.3, tubeWidth, rectangleWidth * 0.55, tubeLightGlowColor.cssColor, cornerRadius),
+        new LightTube(-rectangleStart, -rectangleStart * 0.3, tubeWidth, rectangleWidth * 0.55, tubeLightGlowColor, cornerRadius),
 
     ];
     verticalLightTubes.forEach((lightTube) => {
@@ -123,20 +123,20 @@ const drawLightingRig = (sketch: p5, tubeLightGlowColor: { cssColor: string }) =
     sketch.rotate(90)
     let horizontalLightTubes = [
         // top
-        new LightTube(rectangleStart, rectangleStart, tubeWidth, rectangleWidth * 0.7, tubeLightGlowColor.cssColor, cornerRadius),
-        new LightTube(rectangleStart, -rectangleStart * 0.5, tubeWidth, rectangleWidth * 0.2, tubeLightGlowColor.cssColor, cornerRadius),
+        new LightTube(rectangleStart, rectangleStart, tubeWidth, rectangleWidth * 0.7, tubeLightGlowColor, cornerRadius),
+        new LightTube(rectangleStart, -rectangleStart * 0.5, tubeWidth, rectangleWidth * 0.2, tubeLightGlowColor, cornerRadius),
         // top middle
-        new LightTube(-rectangleWidth * 0.25, -rectangleWidth * 0.3, tubeWidth, rectangleWidth * 0.9, tubeLightGlowColor.cssColor, cornerRadius),
-        new LightTube(-rectangleWidth * 0.25, rectangleWidth * 0.5, tubeWidth, rectangleWidth * 0.3, tubeLightGlowColor.cssColor, cornerRadius),
+        new LightTube(-rectangleWidth * 0.25, -rectangleWidth * 0.3, tubeWidth, rectangleWidth * 0.9, tubeLightGlowColor, cornerRadius),
+        new LightTube(-rectangleWidth * 0.25, rectangleWidth * 0.5, tubeWidth, rectangleWidth * 0.3, tubeLightGlowColor, cornerRadius),
         // middle
-        new LightTube(0, rectangleStart * 0.5, tubeWidth, rectangleWidth * 0.35, tubeLightGlowColor.cssColor, cornerRadius),
-        new LightTube(0, -rectangleStart * 1.1, tubeWidth, rectangleWidth * 0.35, tubeLightGlowColor.cssColor, cornerRadius),
+        new LightTube(0, rectangleStart * 0.5, tubeWidth, rectangleWidth * 0.35, tubeLightGlowColor, cornerRadius),
+        new LightTube(0, -rectangleStart * 1.1, tubeWidth, rectangleWidth * 0.35, tubeLightGlowColor, cornerRadius),
         // bottom middle
-        new LightTube(rectangleWidth * 0.25, -rectangleStart * 0.2, tubeWidth, rectangleWidth * 0.5, tubeLightGlowColor.cssColor, cornerRadius),
+        new LightTube(rectangleWidth * 0.25, -rectangleStart * 0.2, tubeWidth, rectangleWidth * 0.5, tubeLightGlowColor, cornerRadius),
         // bottom
-        new LightTube(-rectangleStart, rectangleStart, tubeWidth, rectangleWidth * 0.3, tubeLightGlowColor.cssColor, cornerRadius),
-        new LightTube(-rectangleStart, 0, tubeWidth, rectangleWidth * 0.25, tubeLightGlowColor.cssColor, cornerRadius),
-        new LightTube(-rectangleStart, -rectangleStart, tubeWidth, rectangleWidth * 0.25, tubeLightGlowColor.cssColor, cornerRadius),
+        new LightTube(-rectangleStart, rectangleStart, tubeWidth, rectangleWidth * 0.3, tubeLightGlowColor, cornerRadius),
+        new LightTube(-rectangleStart, 0, tubeWidth, rectangleWidth * 0.25, tubeLightGlowColor, cornerRadius),
+        new LightTube(-rectangleStart, -rectangleStart, tubeWidth, rectangleWidth * 0.25, tubeLightGlowColor, cornerRadius),
     ];
     horizontalLightTubes.forEach((lightTube) => {
         drawLightTube(sketch, lightTube.x, lightTube.y, lightTube.width, lightTube.height, lightTube.glowColor, lightTube.cornerRadius);
@@ -145,13 +145,50 @@ const drawLightingRig = (sketch: p5, tubeLightGlowColor: { cssColor: string }) =
 
     // place lights on brackets
 }
-let tubeLightGlowColor = { cssColor: "#ff0000" };
 
+let settings = {
+    lightingRigs: {
+        left: {
+            scale: 0.5,
+            x: -700,
+            y: -300,
+            rotation: 0
+        },
+        middle: {
+            scale: 0.5,
+            x: 0,
+            y: 200,
+            rotation: 270
+        },
+        right: {
+            scale: 0.5,
+            x: 700,
+            y: -100,
+            rotation: 180
+        }
+    },
+    tubeLightGlowColor: "#ff0000"
+}
 
 const sketch = (sketch: p5) => {
     sketch.setup = function () {
         let customizeSketchGui = new GUI();
-        customizeSketchGui.addColor(tubeLightGlowColor, 'cssColor');
+        customizeSketchGui.addColor(settings, 'tubeLightGlowColor');
+        const lightingRigLeftGui = customizeSketchGui.addFolder('Lighting Rig Left');
+        lightingRigLeftGui.add(settings.lightingRigs.left, 'x', -1000, 1000, 10);
+        lightingRigLeftGui.add(settings.lightingRigs.left, 'y', -1000, 1000, 10);
+        lightingRigLeftGui.add(settings.lightingRigs.left, 'rotation', 0, 360, 10);
+        lightingRigLeftGui.add(settings.lightingRigs.left, 'scale', 0, 1, 0.01);
+        const lightingRigMiddleGui = customizeSketchGui.addFolder('Lighting Rig Middle');
+        lightingRigMiddleGui.add(settings.lightingRigs.middle, 'x', -1000, 1000, 10);
+        lightingRigMiddleGui.add(settings.lightingRigs.middle, 'y', -1000, 1000, 10);
+        lightingRigMiddleGui.add(settings.lightingRigs.middle, 'rotation', 0, 360, 10);
+        lightingRigMiddleGui.add(settings.lightingRigs.middle, 'scale', 0, 1, 0.01)
+        const lightingRigRightGui = customizeSketchGui.addFolder('Lighting Rig Right');
+        lightingRigRightGui.add(settings.lightingRigs.right, 'x', -1000, 1000, 10);
+        lightingRigRightGui.add(settings.lightingRigs.right, 'y', -1000, 1000, 10);
+        lightingRigRightGui.add(settings.lightingRigs.right, 'rotation', 0, 360, 10);
+        lightingRigRightGui.add(settings.lightingRigs.right, 'scale', 0, 1, 0.01);
         sketch.angleMode(sketch.DEGREES);
         sketch.createCanvas(sketch.windowWidth, sketch.windowHeight);
     }
@@ -159,25 +196,28 @@ const sketch = (sketch: p5) => {
     sketch.draw = function () {
         sketch.background(0);
         sketch.translate(sketch.width / 2, sketch.height / 2);
-        sketch.scale(0.5);
         // draw lighting rig on the left
         sketch.push();
-        sketch.translate(-700, -300)
-        drawLightingRig(sketch, tubeLightGlowColor);
+        sketch.scale(settings.lightingRigs.left.scale);
+        sketch.translate(settings.lightingRigs.left.x, settings.lightingRigs.left.y)
+        sketch.rotate(settings.lightingRigs.left.rotation);
+        drawLightingRig(sketch, settings.tubeLightGlowColor);
         sketch.pop();
 
         // draw lighting rig in the middle
         sketch.push();
-        sketch.translate(0, 200)
-        sketch.rotate(270);
-        drawLightingRig(sketch, tubeLightGlowColor);
+        sketch.scale(settings.lightingRigs.middle.scale);
+        sketch.translate(settings.lightingRigs.middle.x, settings.lightingRigs.middle.y)
+        sketch.rotate(settings.lightingRigs.middle.rotation);
+        drawLightingRig(sketch, settings.tubeLightGlowColor);
         sketch.pop()
 
         // draw lighting rig on the right
         sketch.push();
-        sketch.translate(700, -100)
-        sketch.rotate(180);
-        drawLightingRig(sketch, tubeLightGlowColor);
+        sketch.scale(settings.lightingRigs.right.scale);
+        sketch.translate(settings.lightingRigs.right.x, settings.lightingRigs.right.y)
+        sketch.rotate(settings.lightingRigs.right.rotation);
+        drawLightingRig(sketch, settings.tubeLightGlowColor);
         sketch.pop();
     }
 
